5cd9c4ba3432f747fdd48ffe00b7ec32c4b0878a
FaizanAnsari185 <superseupermfa@gmail.com>
2024-06-11 18:11:52 +0530

    fix

--- app/page.jsx
+++ app/page.jsx
@@ -1,4 +1,12 @@
-import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle, SheetTrigger } from "@/components/ui/sheet";
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
 import { Slider } from "@/components/ui/slider";
 import { Switch } from "@/components/ui/switch";
 import React from "react";
@@ -7,7 +15,9 @@ export default function Home() {
   return (
     <div>
       <Sheet>
-        <SheetTrigger className="font-bold border-black p-2 m-2 border-2">Open</SheetTrigger>
+        <SheetTrigger className="font-bold border-black p-2 m-2 border-2">
+          Open
+        </SheetTrigger>
         <SheetContent side="left">
           <SheetHeader>
             <SheetTitle>Are you absolutely sure?</SheetTitle>
@@ -20,9 +30,18 @@ export default function Home() {
         </SheetContent>
       </Sheet>
 
-      <Switch/>
+      <Switch />
 
       <Slider defaultValue={[33]} max={100} step={1} />
+
+      <Accordion type="single" collapsible>
+        <AccordionItem value="item-1">
+          <AccordionTrigger>Is it accessible?</AccordionTrigger>
+          <AccordionContent>
+            Yes. It adheres to the WAI-ARIA design pattern.
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
 
 
     </div>
