6910d51f78c1a3b247c2adc430bc6fb7cdfa06d3
FaizanAnsari185 <superseupermfa@gmail.com>
2024-06-11 17:32:05 +0530

    add components from shadcn

---
+++ app/page.jsx
@@ -0,0 +1,30 @@
+import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle, SheetTrigger } from "@/components/ui/sheet";
+import { Slider } from "@/components/ui/slider";
+import { Switch } from "@/components/ui/switch";
+import React from "react";
+
+export default function Home() {
+  return (
+    <div>
+      <Sheet>
+        <SheetTrigger className="font-bold border-black p-2 m-2 border-2">Open</SheetTrigger>
+        <SheetContent side="left">
+          <SheetHeader>
+            <SheetTitle>Are you absolutely sure?</SheetTitle>
+            <SheetDescription>
+              This action cannot be undone. This will permanently delete your
+              account and remove your data from our servers.
+            </SheetDescription>
+          </SheetHeader>
+          <div>faizan</div>
+        </SheetContent>
+      </Sheet>
+
+      <Switch/>
+
+      <Slider defaultValue={[33]} max={100} step={1} />
+
+
+    </div>
+  );
+}
